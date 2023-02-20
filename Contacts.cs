7b68477305433a630fd602745c7a78397548e8f7
FIRST_NAME LAST_NAMEsanketppatil99 <sanketppatil99@gmail.com>
2023-02-20 11:44:20 +0530

    Ability to create contacts

---
+++ Contacts.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    public class Contacts
+    {
+        public string fName { get; set; }
+        public string lName { get; set; }
+        public string address { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public int zipcode { get; set; }
+        public long phoneNumber { get; set; }
+        public string email { get; set; }
+
+    }
+}
