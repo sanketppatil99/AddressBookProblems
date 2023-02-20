18494d4560773809b53c6f0b2217d7d222ba9e45
FIRST_NAME LAST_NAMEsanketppatil99 <sanketppatil99@gmail.com>
2023-02-20 11:47:22 +0530

    Ability to create contacts

--- Program.cs
+++ Program.cs
@@ -5,6 +5,8 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to address Book System");
+            AddressBookMain addressBookMain = new AddressBookMain();
+            addressBookMain.CreatContact();
         }
     }
 }
