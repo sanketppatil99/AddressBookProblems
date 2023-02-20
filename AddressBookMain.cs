bbf186292b40cc796aed44518c912330ae400fea
FIRST_NAME LAST_NAMEsanketppatil99 <sanketppatil99@gmail.com>
2023-02-20 11:45:06 +0530

    Ability to create contacts

---
+++ AddressBookMain.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    public class AddressBookMain
+    {
+        Contacts contact = new Contacts();
+        public void CreatContact()
+        {
+            Console.WriteLine("Enter Your First Name : ");
+            contact.fName = Console.ReadLine();
+            Console.WriteLine("Enter Your Last Name : ");
+            contact.lName = Console.ReadLine();
+            Console.WriteLine("Enter Your Address : ");
+            contact.address = Console.ReadLine();
+            Console.WriteLine("Enter Your City : ");
+            contact.city = Console.ReadLine();
+            Console.WriteLine("Enter Your State : ");
+            contact.state = Console.ReadLine();
+            Console.WriteLine("Enter Your Zip Code : ");
+            contact.zipcode = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Your Phone No : ");
+            contact.phoneNumber = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine("Enter Your Email : ");
+            contact.email = Console.ReadLine();
+        }
+    }
+}
