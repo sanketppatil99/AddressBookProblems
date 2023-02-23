faf0b19c0f782cee989768c35f4400acc24f2578
FIRST_NAME LAST_NAMEsanketppatil99 <sanketppatil99@gmail.com>
2023-02-23 06:51:45 +0530

    Add new Contacts

--- Program.cs
+++ Program.cs
@@ -2,8 +2,10 @@
 {
     class Program
     {
+        public static List<Contacts> Person = new List<Contacts>();
         public static void Main(string[] args)
         {
+
             Console.WriteLine("Welcome to address Book System");
             AddressBookMain addressBookMain = new AddressBookMain();
             addressBookMain.CreatContact();
