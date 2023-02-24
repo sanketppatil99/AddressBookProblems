49f9e9a2fcb3fcf9a024abb3280a55c2f9c141f3
FIRST_NAME LAST_NAMEsanketppatil99 <sanketppatil99@gmail.com>
2023-02-25 00:05:03 +0530

    Edit existing contact person using their name

--- Program.cs
+++ Program.cs
@@ -7,8 +7,34 @@
         {
 
             Console.WriteLine("Welcome to address Book System");
-            AddressBookMain addressBookMain = new AddressBookMain();
-            addressBookMain.CreatContact();
+            AddressBookMain.CreateContacts();
+
+
+            bool end = true;
+            while (true)
+            {
+                Console.WriteLine("Select Option\n1.add Contact \n2.Display \n3.Edit Contact \n4.end");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        AddressBookMain.CreateContacts();
+                        break;
+                    case 2:
+                        AddressBookMain.Dispaly();
+                        break;
+                    case 3:
+
+                        AddressBookMain.EditContact();
+                        break;
+                    case 4:
+                        end = false;
+                        break;
+                    default:
+                        Console.WriteLine("Incorrect Option");
+                        break;
+                }
+            }
         }
     }
 }
