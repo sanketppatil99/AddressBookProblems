f60d93f9df250805f0877b17a2ffd5b9fb3eecb6
FIRST_NAME LAST_NAMEsanketppatil99 <sanketppatil99@gmail.com>
2023-02-25 00:06:16 +0530

    Edit existing contact person using their name

--- AddressBookMain.cs
+++ AddressBookMain.cs
@@ -8,25 +8,92 @@ namespace AddressBook
 {
     public class AddressBookMain
     {
-        Contacts contact = new Contacts();
-        public void CreatContact()
+        public static Contacts contacts = new Contacts();
+        public static List<Contacts> Person = new List<Contacts>();
+        public static void CreateContacts()
         {
-            Console.WriteLine("Enter Your First Name : ");
-            contact.fName = Console.ReadLine();
-            Console.WriteLine("Enter Your Last Name : ");
-            contact.lName = Console.ReadLine();
-            Console.WriteLine("Enter Your Address : ");
-            contact.address = Console.ReadLine();
-            Console.WriteLine("Enter Your City : ");
-            contact.city = Console.ReadLine();
-            Console.WriteLine("Enter Your State : ");
-            contact.state = Console.ReadLine();
-            Console.WriteLine("Enter Your Zip Code : ");
-            contact.zipcode = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter Your Phone No : ");
-            contact.phoneNumber = Convert.ToInt64(Console.ReadLine());
-            Console.WriteLine("Enter Your Email : ");
-            contact.email = Console.ReadLine();
+            
+            Console.WriteLine("Enter First Name : ");
+            contacts.fName = Console.ReadLine();
+
+            Console.WriteLine("Enter Last Name : ");
+            contacts.lName = Console.ReadLine();
+
+            Console.WriteLine("Enter address : ");
+            contacts.address = Console.ReadLine();
+
+            Console.WriteLine("Enter City : ");
+            contacts.city = Console.ReadLine();
+
+            Console.WriteLine("Enter State : ");
+            contacts.state = Console.ReadLine();
+
+            Console.WriteLine("Enter ZipCode : ");
+            contacts.zipcode = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Phone Number : ");
+            contacts.phoneNumber = Convert.ToInt64(Console.ReadLine());
+
+            Console.WriteLine("Enter Email : ");
+            contacts.email = Console.ReadLine();
+            Program.Person.Add(contacts);
+
+        }
+
+        public static void Dispaly()
+        {
+            foreach (var contact in Program.Person)
+            {
+                Console.WriteLine("First Name : " + contact.fName + "\nLast Name : " + contact.lName + "\naddress : " + contact.address + "\nCity : " + contact.city + "\nState : " + contact.state + "\nZipCode : " + contact.zipcode + "\nPhone Number : " + contact.phoneNumber + "\nEmail : " + contact.email);
+            }
+        }
+        public static void EditContact()
+        {
+            Console.WriteLine("Edit a contact list enter first name");
+
+            string name = Console.ReadLine();
+
+            if (contacts.fName == name)
+            {
+                Console.WriteLine("Edit a Contact\n 1.lastName\n 2.Address\n 3.City\n 4.State\n 5.Zip\n 6.phone no\n 7.email");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        string lastName = Console.ReadLine();
+                        contacts.lName = lastName;
+                        break;
+                    case 2:
+                        string address = Console.ReadLine();
+                        contacts.address = address;
+                        break;
+                    case 3:
+                        string city = Console.ReadLine();
+                        contacts.city = city;
+                        break;
+                    case 4:
+                        string state = Console.ReadLine();
+                        contacts.state = state;
+                        break;
+                    case 5:
+                        string zipcode = Console.ReadLine();
+                        contacts.zipcode = Convert.ToInt32(zipcode);
+                        break;
+                    case 6:
+                        string phoneNo = Console.ReadLine();
+                        contacts.phoneNumber = Convert.ToInt64(Console.ReadLine());
+                        break;
+                    case 7:
+                        string email = Console.ReadLine();
+                        contacts.email = email;
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Name is not Exist");
+            }
+
         }
     }
 }
